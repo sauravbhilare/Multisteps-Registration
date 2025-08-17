60bb6b6a650158d14a0fb1c6450ffbbb1a368a12
Saurav Bhilare <sauravbhilare0403@gmail.com>
2025-08-17 21:50:49 +0530

    Deploy Commit New

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
